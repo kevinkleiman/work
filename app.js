d50e4019823868ddf1316f7fc89a4b43cc1b4356
Kevin <kxkleiman@gmail.com>
2019-10-04 14:10:03 -0600

    Added pug file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res){
-   res.send('Hello World')
+   res.render('index', {title: 'hello'});
 });
 
 app.listen(3000, function () {
